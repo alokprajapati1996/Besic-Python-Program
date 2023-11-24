54bbaea0a76e679f6033d87316a4508f59fb413b
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-11-24 06:57:41 +0530

    [Alok] Added:code check largest value

---
+++ if_else_statement.py
@@ -0,0 +1,19 @@
+'''
+
+@Author: Alok kumar
+
+@Date: 24-11-23 6:00:30
+
+@Last Modified by: Alok kumar
+
+@Last Modified time: 24-11-2023 6:00:30
+
+@Title : Besic-python-program
+
+'''
+a=int(input("Enter value a: "))
+b=int(input("Enter value b:  "))
+if a>b:
+    print("a is largest value.")
+else:
+    print("b is largest.")
