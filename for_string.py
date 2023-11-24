6727f7df59ca7d5be8e093877057d158ec69e789
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-11-24 10:08:48 +0530

    [Alok] Added:code print one to ten use while loop

---
+++ for_string.py
@@ -0,0 +1,25 @@
+'''
+
+@Author: Alok kumar
+
+@Date: 24-11-23 6:00:30
+
+@Last Modified by: Alok kumar
+
+@Last Modified time: 24-11-2023 6:00:30
+
+@Title : Besic-python-program
+
+'''
+for x in "Alok":
+    print(x)
+
+
+
+
+
+
+
+
+
+
---
+++ while_loop.py
@@ -0,0 +1,18 @@
+'''
+
+@Author: Alok kumar
+
+@Date: 24-11-23 6:00:30
+
+@Last Modified by: Alok kumar
+
+@Last Modified time: 24-11-2023 6:00:30
+
+@Title : Besic-python-program
+
+'''
+i=1
+n=10
+while i<=n:
+    print(i)
+    i=i+1
