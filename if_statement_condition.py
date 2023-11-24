b29068767ee5d1e46dac414f489ecbaf59ed43db
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-11-24 06:32:23 +0530

    [Alok] Added:code check eligibility for vote

---
+++ if_statement_condition.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Alok kumar
+
+@Date: 24-11-23 6:00:30
+
+@Last Modified by: Alok kumar
+
+@Last Modified time: 24-11-2023 6:00:30
+
+@Title : Besic-python-program
+
+'''
+
+age=int(input("enter first name: "))
+if age>=18:
+    print("you are eligible for vote")
