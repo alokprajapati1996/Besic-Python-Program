cd3e30ff36099198f3c434679e4fce4cc60c7d89
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-11-24 09:02:06 +0530

    [Alok] Added:code check condition by elif

---
+++ elif_statement.py
@@ -0,0 +1,19 @@
+'''
+
+@Author: Alok kumar
+
+@Date: 24-11-23 9:00:30
+
+@Last Modified by: Alok kumar
+
+@Last Modified time: 24-11-2023 9:00:30
+
+@Title : Besic-python-program
+
+'''
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
