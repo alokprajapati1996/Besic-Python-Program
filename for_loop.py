5bfbfafa15720914b1b85b448598b75aaf15076f
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-11-24 09:30:58 +0530

    [Alok] Added:code iterate use for loop

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+
+'''
+
+@Author: Alok kumar
+
+@Date: 24-11-23 6:00:30
+
+@Last Modified by: Alok kumar
+
+@Last Modified time: 24-11-2023 6:00:30
+
+@Title : Besic-python-program
+
+'''
+intro = ["My", "name", "Alok","26","year","old"]
+for x in intro:
+  print(x)
