0054314ae158caa1a90543814b3f46ecea0a7eb3
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-11-27 10:24:11 +0530

    [Alok]Added:code flip coin head or tails

---
+++ check_coin_head_tail.py
@@ -0,0 +1,19 @@
+'''
+
+@Author: Alok kumar
+
+@Date: 27-11-23 9:00:30
+
+@Last Modified by: Alok kumar
+
+@Last Modified time: 27-11-2023 10:21:30
+
+@Title : Besic-python-program
+
+'''
+import random
+coin=(random.uniform(0,1))
+if coin>0.5:
+    print("coin head")
+else:
+    print("coin tails")
