aec8e81040f4a72954ee0d68b2b406f2baa53b67
Alok kumar Prajapti <alokprajapati913@gmail.com>
2023-11-29 10:06:16 +0530

    [Alok]Added:code tuple practice

--- tuple.py
+++ tuple.py
@@ -1,3 +1,16 @@
+
+"""
+@Author: Alok kumar
+
+@Date: 24-11-23 9:00:30
+
+@Last Modified by: Alok kumar
+
+@Last Modified time: 24-11-2023 9:00:30
+
+@Title : tuple practice all the function
+
+"""
 tuple1=(1,2,3,4,5)
 print(tuple1)
 print(len(tuple1))
@@ -8,3 +21,22 @@ print(tuple1[-5:-1])
 #chek number exist or not
 if 4 in tuple1:
     print("yes exist")
+#check tuple length
+print(type(tuple1))
+
+#access tuple itam
+print(tuple1[1])
+
+#negative index
+print(tuple1[-1])
+
+#range of item lik slicing
+print(tuple1[0:3])
+
+#update tuple value
+x = ("apple", "banana", "orange")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
